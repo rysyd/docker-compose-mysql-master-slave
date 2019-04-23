0ef2ab44f4c19f0dcd6c31c1ea95573d83dfad7b
hlwojiv <hlwojiv@gmail.com>
2019-04-23 11:57:34 +0800

    删除 .DS 文件

--- mysql_connector.sh
+++ mysql_connector.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-echo "* 等待 MySQL 启动……"
-# Give 60 seconds for master and slave to come up
+echo "等待 MySQL 启动"
 sleep 60
 
-echo "* master 创建同步用户……"
+echo "master 创建同步用户"
 
 mysql --host 10.20.1.2 -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY '123456';"
 mysql --host 10.20.1.2 -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'flush privileges;'
 
 
-echo "* 设置从库"
+echo "slave 设置"
 
 Master_Position=$(eval "mysql --host 10.20.1.2 -uroot -p$MYSQL_MASTER_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
 Master_File=$(eval "mysql --host 10.20.1.2 -uroot -p$MYSQL_MASTER_PASSWORD -e 'show master status \G'     | grep File     | sed -n -e 's/^.*: //p'")
@@ -20,18 +19,18 @@ mysql --host 10.20.1.3 -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "CHANGE MASTER TO m
         master_log_pos=$Master_Position;"
 
 
-echo "* 启动 slave"
+echo "slave 开启"
 mysql --host 10.20.1.3 -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "start slave;"
 
-echo "Increase the max_connections to 2000"
+echo "设置 max_connections 参数为 2000"
 mysql --host 10.20.1.2 -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
 mysql --host 10.20.1.3 -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
 
-mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e "show slave status \G"
+echo "master 状态"
+mysql --host 10.20.1.2 -uroot -p$MYSQL_SLAVE_PASSWORD -e "show master status"
+echo "slave 状态"
+mysql --host 10.20.1.3 -uroot -p$MYSQL_SLAVE_PASSWORD -e "show slave status \G"
 
-echo "----------------------"
-echo "----------------------"
-echo "* MySQL 主从同步已创建 *"
-echo "----------------------"
-echo "----------------------"
+echo "*** MySQL 主从同步已创建 ***"
+
 
